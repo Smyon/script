6612c816120ef14f545375fef27e1fed862e171b
Smyon <bucchyake@gmail.com>
2018-05-27 21:20:05 +0300

    Grid

--- script/script.js
+++ script/script.js
@@ -20,39 +20,67 @@ $(window).scroll(function () {
 
     var scroll = $(this).scrollTop();
     //console.log(scroll);
+    console.log(scroll);
 
     $(".headline").css({
         "transform": "translate(0%, " + scroll / 2.5 + "%)",
         "opacity": 1 - scroll / 300 + ""
     });
-    console.log("hello" + scroll);
-
     $(".subhead").css({
         "transform": "translate(0%, " + scroll + "%)",
         "opacity": 1 - scroll / 300 + ""
     });
 
 
-    if (scroll >= 550 && scroll <= 1000) {
+    if (scroll >= 600 && scroll <= 1000) {
         $(".typography-intro-wrapper").css({
-            "transform": "translate(0%, " + (scroll - 550) / -10 + "%)",
+            "transform": "translate(0%, " + (scroll - 600) / -10 + "%)",
             "opacity": (scroll - 600) / 300
+        });
+    } else if (scroll >= 1000) {
+        $(".typography-intro-wrapper").css({
+            "transform": "translate(0%, -39.8%)",
+
+            "opacity": 1
         });
     }
 
-    if (scroll >= 1100 && scroll <= 1400) {
+
+    if (scroll >= 1000 && scroll <= 1800) {
+
+
         $(".section-product-opacity").css({
-            "opacity": (scroll - 1100)  / 300
+            "opacity": (scroll - 1450)  / 250
 
         });
 
         $(".section-product-opacity-zero").css({
-            "opacity": 1 - (scroll - 1100)  / 300
+            "opacity": 1 - (scroll - 1450)  / 250
 
+        });
+black
+black
+black
+black
+black
+black
+black
+black
+black
+black
+black
+    }
+
+    if (scroll >= 1100 && scroll <= 1800) {
+        //console.log("hello" + scroll);
+
+        $(".product-headline").css({
+
+            "opacity": (scroll - 1300)  / 250 + 0.7,
+            "transform": "translate(0%, " + (scroll - 1300) / -5 + "%)"
         });
 
     }
-
 
     // console.log(value);
 
